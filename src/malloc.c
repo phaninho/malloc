c40a768e3662f407121738f5b2804cf420158767
Stephane MARTINS <stmartin@e1r5p23.42.fr>
2018-01-19 23:11:52 +0100

    comment init function

--- src/malloc.c
+++ src/malloc.c
@@ -3,21 +3,20 @@
 
 void    init_tiny_page(t_page_tiny *block)
 {
-  ft_memset(block, 0, sizeof(t_page_tiny));
+  (void)block;
+  // ft_memset(block, 0, TINY_BLOCK_SIZE);
 }
 
-void          *tiny_alloc(size_t size)
+void          *tiny_alloc(size_t size, t_page_tiny *block)
 {
-  void          *ptr;
-  t_page_tiny   block;
-(void)size;
-  ptr = NULL;
-  init_tiny_page(&block);
+  (void)size;
+  // ptr = NULL;
+  init_tiny_page(block);
   printf("c'est un TINY\n");
-  if ((ptr = (void *)mmap(0, TINY_BLOCK_SIZE, PROT_READ | PROT_WRITE,\
-    MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
+  // if ((block = (void *)mmap(0, TINY_BLOCK_SIZE, PROT_READ | PROT_WRITE,
+    //MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
     return (NULL);
-  return (ptr);
+  return (block);
 }
 
 void          *small_alloc(size_t size)
@@ -46,16 +45,17 @@ void          *large_alloc(size_t size)
 
 void	       *malloc(size_t size)
 {
-  void     *ptr;
-  //t_block  *block;
+  void          *ptr;
+  t_page_tiny   *block;
 
-  printf("MTS=>%zu, MSS=>%zu t_page_tiny=>[%zu]  t_page_small=>[%zu]\n", META_SIZE_TINY, META_SIZE_SMALL, sizeof(t_page_tiny), sizeof(t_page_small));
+  block = NULL;
+  printf("MTS=>%zu, MSS=>%zu t_page_tiny=>[%zu]  t_page_small=>[%zu]\n", TINY_BLOCK_SIZE, SMALL_BLOCK_SIZE, sizeof(t_page_tiny), sizeof(t_page_small));
   size = align4(size);
   printf("apres align4, size => [%zu], small %d\n", size, SMALL);
   if (size == 0)
     return (NULL);
   else if(size <= TINY)
-    ptr = tiny_alloc(size);
+    ptr = tiny_alloc(size, block);
   else if (size <= SMALL)
     ptr = small_alloc(size);
   else
